977c3a60daa6d5bbf178bbd0f547832bd109898d
upadhyayniti <niti_u@hotmail.com>
2022-09-21 16:35:53 -0400

    updated config for backend access

--- src/vue.config.js
+++ src/vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
     devServer: {
         proxy: {
          '^api': {
-            target: 'http://localhost:5000',
+            target: 'http://backend.svc.cluster.local:5000',
             changeOrigin: true,
             logLevel: 'debug',
             pathRewrite: { '^api': '/'},
